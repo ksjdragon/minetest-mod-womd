eb37f327a3e046156c23e42ae8a042709a18e1f3
Yaman <ybq987@gmail.com>
2015-01-10 12:53:37 -0500

    added some stuff

--- init.lua
+++ init.lua
@@ -1,14 +1,12 @@
 minetest.register_node("womd:radioactive_air", {
-	inventory_image = "unknown_node.png",
-	drawtype = "airlike",
+	tiles = {"radioactive_air.png"},
+	drawtype = "glasslike",
 	paramtype = "none",
-	sunlight_propagates = false,
 	walkable = false,
 	pointable = false,
 	diggable = false,
 	air_equivalent = true,
 	drop = "",
 	damage_per_second = 1,
-	liquid_viscosity = 4,
 	groups = {not_in_creative_inventory=1},
 })
